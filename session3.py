b7ab7986a8b26d6450be62b2577f2a19793cb178
Aima <shabrangfatemeh83@gmail.com>
2025-12-30 18:20:34 +0330

    continue changes

--- session3.py
+++ session3.py
@@ -1,13 +1,18 @@
 import random
-def engine(temp):
+import time
+def engine_temp():
+       temp = random.randit(50, 120)
+       print("random generated temp number:", 50 to 120)
        return engine_temp
-       print("random generated temp number")
 while True:
-    i = engine_temp = int("a random number between 50 to 120")
-    print("a random number between:", 50 < i > 120)
+    i = engine_temp 
+    if 50 <= i => 120:
+        print("a random number between:", 50 < i > 120)
+    else:
+         print("out of range:", i)
     if i > 100:
         print("WARNING! down engine_temp")
-import datetime.sleep(1)
+    time.sleep(1)
 print("after 1 sec engine_temp")
     
 
