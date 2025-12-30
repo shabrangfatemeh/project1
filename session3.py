dccd81f1c0f449be25516251798cbd865bff46f9
Aima <shabrangfatemeh83@gmail.com>
2025-12-30 17:59:09 +0330

    start changes

---
+++ session3.py
@@ -0,0 +1,13 @@
+import random
+def engine(temp):
+       return engine_temp
+       print("random generated temp number")
+while True:
+    i = engine_temp = int("a random number between 50 to 120")
+    print("a random number between:", 50 < i > 120)
+    if i > 100:
+        print("WARNING! down engine_temp")
+import datetime.sleep(1)
+print("after 1 sec engine_temp")
+    
+
