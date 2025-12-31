63aee81e3493326caea9ca58c58eaf547823b7a7
Aima <shabrangfatemeh83@gmail.com>
2025-12-31 08:04:03 +0330

    continue changes3

--- session3.py
+++ session3.py
@@ -1,23 +1,21 @@
 import random
 import time
-def status(engine_temp, buttry_level):
+def status():
     engine_temp = random.randint(50, 120)
-    return engine_temp
     buttry_level = random.randint(0, 100)
-    return buttry_level   
+    return buttry_level, engine_temp   
 while True:
-    i = engine_temp =15
-    b = buttry_level =105
+    buttry_level, engine_temp = status()
     print("random generated engine_temp:", engine_temp)
     print("random generated buttry_level:", buttry_level)
-    if b < 15 and i > 105:
+    if buttry_level < 15 and engine_temp > 105:
         print("EMERGENCY stop!")
-    if i > 100:
-        print("WARNING! down engine_temp")
-    for buttry_level in (0, 100):
-        if b < 20:
-        print("WARNING:Low Buttery! Finding charger")
         break
+    if engine_temp > 100:
+        print("WARNING! down engine_temp")
+    if buttry_level < 20:
+        print("WARNING:Low Buttery! Finding charger")
+       
 time.sleep(5)
 print("after 5 sec ...\n")
     
