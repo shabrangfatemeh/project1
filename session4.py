e8bbe25ffe9035f170a3983a70dd75aa3ad74cf1
Aima <shabrangfatemeh83@gmail.com>
2025-12-31 16:10:06 +0330

    changes5-4

--- session4.py
+++ session4.py
@@ -1,5 +1,5 @@
 import json
-car_data = {"car_id": "bot_01", "battery": 112, "engine_temp": 112, 
+car_data = {"car_id": "bot_01", "battery": 85, "engine_temp": 112, "speed": 40, 
             "status": "moving", "location": {"lat": 35.6892, "lng":51.3890}
             }
 report = json.dumps(car_data, indent=4)
@@ -7,3 +7,5 @@ print("---copy the json below and send it to me ---")
 with open("car_report.json", "w") as f:
     json.dump(car_data, f, indent=4)
     print("status: data saved to file successfully!")
+import os
+print("file path is:", os.getcwd())
