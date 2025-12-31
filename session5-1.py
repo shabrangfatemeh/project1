5dc6ac329cb2dd2c8b1f69c784fd49dd67c099a1
Aima <shabrangfatemeh83@gmail.com>
2025-12-31 17:12:36 +0330

    changes5-1-1

---
+++ session5-1.py
@@ -0,0 +1,8 @@
+import json
+with open("car_report.json", "r") as f:
+    recovered_data = json.load(f)
+print("--- data_recovered successfully ---")
+print(f"car ID: {recovered_data['car_id']}")
+print(f"current battery: {recovered_data['battery']}%")
+if recovered_data['battrey'] < 20:
+    print("action required: low battery detected in history!")    
