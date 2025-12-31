914238c2ef6a5de5dd91d6d14d8163b425de53ac
Aima <shabrangfatemeh83@gmail.com>
2025-12-31 11:16:19 +0330

    changes4-4

--- session4.py
+++ session4.py
@@ -4,4 +4,6 @@ car_data = {"car_id": "bot_01", "battery": 112, "engine_temp": 112,
             }
 report = json.dumps(car_data, indent=4)
 print("---copy the json below and send it to me ---")
-print(report)
+with open("car_report.json", "w") as f:
+    json.dump(car_data, f, indent=4)
+    print("status: data saved to file successfully!")
