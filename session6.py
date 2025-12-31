ecd7991c9ae813b78033ec2e60fc7063bea934c4
Aima <shabrangfatemeh83@gmail.com>
2025-12-31 21:00:50 +0330

    changes6-2

--- session6.py
+++ session6.py
@@ -2,13 +2,16 @@ import json
 
 
 driver_name = input("what is your name?:")
-print("hi, wellcom driver_name")
+print("hi, wellcom {driver_name}")
 
-battery_driver = input(int("how much percentage of charge was added?: "))
+battery_driver = int(input("how much percentage of charge was added?: "))
 print("battery_driver", battery_driver)
 
-def add(battery_driver):
-    print(f"battery driver:", {battery_driver + 85})
+def add(battery_added_percent):
+    increased = battery_added_percent + 85
+    print(f"battery driver:", {increased})
 
     print("report")
-    print("status: data savedto file succssfully!")
+    print("status: data savedto file successfully!")
+    
+add(battery_added_percent) 
