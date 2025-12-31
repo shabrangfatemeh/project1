bd2183cfa647d046153dcc439a409604399e0f05
Aima <shabrangfatemeh83@gmail.com>
2025-12-31 10:27:11 +0330

    changes2-4

--- session4.py
+++ session4.py
@@ -1,6 +1,6 @@
 import json
 car_data = {"car_id": "bot_01", "battery": 112,"engine_temp": 112,
             "status": "moving", "location": {"lat": 35.6892, "lang":51.3890}}
-report = json.dumps(car_datd, indent=4)
+report = json.dumps(car_data, indent=4)
 print("---copy the json below and send it to me ---")
 print(report)
