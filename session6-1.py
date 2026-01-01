9055cb92e9e257b41df3a49794add74152fd0ce1
Aima <shabrangfatemeh83@gmail.com>
2026-01-01 16:49:19 +0330

    changes6-1-1

---
+++ session6-1.py
@@ -0,0 +1,22 @@
+#ماشینی کهزمان رسیدن و خرابی و روغن وآب داره
+import json
+car_data = {"car_id": "bot_02", 
+            "status": {
+             "current_mode": "idle" 
+             "is_active": true, 
+             "charging": false
+             }, 
+            "battery": { 
+             "min_capacity": 15,
+             "max_copacity": 250,
+             "current_level": 85,
+             "unit": "percentage"
+             },
+
+             
+
+
+
+
+
+}
