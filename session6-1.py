a8287faea109470eeb5f531cd784cce7c2e23a93
Aima <shabrangfatemeh83@gmail.com>
2026-01-01 19:27:46 +0330

    changes6-1-4

--- session6-1.py
+++ session6-1.py
@@ -1,7 +1,7 @@
 #ماشینی کهزمان رسیدن و خرابی و روغن وآب داره
 import json
 from datetime import datetime
-i = datetime.now
+i = datetime.now()
 time_string = i.strftime("%H:%M:%S")
 
 car_data = {"car_id": "bot_02", 
@@ -25,15 +25,19 @@ car_data = {"car_id": "bot_02",
             "location": {
              "latitude": 35.6892
              "longitude": 51.3890
-             "last_updated": "2026-01-01T15:30:00Z"   
+             "last_updated": "time_string"   
             },
             "diagnostics":{
              "errors": []   
             },
             "faults": []
             }
-report = json.dumps(car_data, time_string, indent=4)
+report = json.dumps(car_data, indent=4)
 print("--- copy the json below and send it to me ---")
-whit open ("car_report2_json", "w") as f:
-json.dumps (car_data, time_string, f, hndent=4)
-print("--- status: data saved to file successfully! ---")
+with open ("car_report2_json", "w") as f:
+    json.dump (car_data, f, indent=4)
+    print("--- status: data saved to file successfully! ---")
+last_time_str = car_data["location"]["last_updated"]
+translated_time = datetime.strptime(last_time_str, "%Y-%M-%DT%H:%M:%SZ")
+diff = datetime.now() - translated_time
+print(f"{diff}:")
