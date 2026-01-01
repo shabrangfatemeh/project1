bad60d4c9177ef1b897df6f3ac70bcc1dfac68ad
Aima <shabrangfatemeh83@gmail.com>
2026-01-01 17:34:47 +0330

    changes6-1-2

--- session6-1.py
+++ session6-1.py
@@ -5,18 +5,26 @@ car_data = {"car_id": "bot_02",
              "current_mode": "idle" 
              "is_active": true, 
              "charging": false
-             }, 
+            }, 
             "battery": { 
              "min_capacity": 15,
              "max_copacity": 250,
              "current_level": 85,
              "unit": "percentage"
-             },
-
-             
-
-
-
-
-
-}
+            },
+            "max_spead": 350,
+            "engine_temp": 150,
+            "fluids": {
+             "oil_level": 75
+             "water_level": 90   
+            },
+            "location": {
+             "latitude": 35.6892
+             "longitude": 51.3890
+             "last_updated": "2026-01-01T15:30:00Z"   
+            },
+            "diagnostics":{
+             "errors": []   
+            },
+            "faults": []
+            }
