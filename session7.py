60aa7e6560eed8a2993441c439515aead6755864
Aima <shabrangfatemeh83@gmail.com>
2026-01-02 09:25:59 +0330

    changes7-5

--- session7.py
+++ session7.py
@@ -1,21 +1,20 @@
 import json
-white open(""    "r") as f:
-    json.loads() 
-    print("    json file to send me")
-json.load(  , f, indent=4)
-
-
+whit open("car_report_json" "r") as f:
+data = json.load(f)
+battery = ["min_thershold"]
 
 status = "car_moving"
 battery = 25
 while battery > 0:
+    print(f"current battery: %{battery}")
+    if battery <=10
+    print("LOW BATTERY! please find e charging ststion")
     print("status: driving after 100 km, the battery low %5.")
     battery = battery - 5
     if  battery == 0:
         print("CAR STOPPED! need charges.")
 
-        
-white open("  "   "w")as f:
-    json.dumps()
-    print("")
-json.dump(    , f, indent=4)
+
+whit open("car_report_json""w")as f:
+json.dump(data, f, indent=4)
+print("status: data saved to file successfully!")
