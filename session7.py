9adae767bcfe0a157166cd2b1b26ca0378155028
Aima <shabrangfatemeh83@gmail.com>
2026-01-02 08:11:54 +0330

    changes7-1

---
+++ session7.py
@@ -0,0 +1,6 @@
+status = "car_moving"
+i = buttery = 25
+while buttery > 0:
+    print("status: draving after 100 km, the buttery low %5.")
+    if i == 0:
+        print("CAR STOPPED! need charges.")
