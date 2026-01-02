7f2cf1d93e2919eb6bf51abc94f3eccfab81a1ea
Aima <shabrangfatemeh83@gmail.com>
2026-01-02 18:19:19 +0330

    changes8-2

--- session8.py
+++ session8.py
@@ -4,6 +4,24 @@ with open("car_report2_json", "r")as f:
     data = json.load(f)
 min_capacity = data["min_capacity"]
 battery = data["battery"]
+engine_temp = data ["engine_temp"]
 status = "moving"
-while battery <= 5:
-    print()
+while battery >= 5:
+    print(f"current battery: {battery}%")
+    if engine_temp >= engine_temp:
+        print("stop now!")
+    if battery <= min_capacity:
+        print("LOW BATTERY, please find charging station")
+        print("low battery 10% every 100 km")
+        battery = battery - 10
+    if battery = 0:
+       battery = 0
+       print("STOP CAR! NO CHARG")
+    
+   
+
+data["battery"] = battery
+data ["engine_temp"] = engine_temp 
+with open("car_report2_json", "w")as f:
+     json.dump(data, f, indent=4)
+print("status: data saved to file successfully!")                
