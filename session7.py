b041d82198534d4e3842ff200f5eb1878374212c
Aima <shabrangfatemeh83@gmail.com>
2026-01-02 08:23:28 +0330

    changes7-3

--- session7.py
+++ session7.py
@@ -1,7 +1,7 @@
 status = "car_moving"
-i = battery = 25
+battery = 25
 while battery > 0:
     print("status: driving after 100 km, the battery low %5.")
     battery = battery - 5
-    if i == 0:
+    if  battery == 0:
         print("CAR STOPPED! need charges.")
