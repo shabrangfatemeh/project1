da996c2fd36bafb00faef3345411d4c14667f2db
Aima <shabrangfatemeh83@gmail.com>
2026-01-02 13:23:19 +0330

    changes7-12

--- session7.py
+++ session7.py
@@ -9,22 +9,22 @@ status = "car_moving"
 
 while battery > 0:
     print(f"current battery: %{battery}")
-    if engine_temp >= engine_temp:
+    if engine_temp >= 110:
         print("STOP!")
         break
 
     if battery <= min_threshold:
-        print("LOW BATTERY! please find a" \
-        " charging ststion")
+        print("LOW BATTERY! please find a charging ststion")
         print("status: driving after 100 km, the battery low %5.")
 
     battery = battery - 5
 
     if  battery == 0:
-        battry = 0
+        battery = 0
         print("CAR STOPPED! need charges.")
 
 data["battery"] = battery
+data["engine_temp"] = engine_temp
 with open("car_report.json", "w")as f:
     json.dump(data, f, indent=4)
 print("status: data saved to file successfully!")
