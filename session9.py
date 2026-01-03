6cf832c9697bb6229f8538a07b74260e6f1b32b1
Aima <shabrangfatemeh83@gmail.com>
2026-01-03 11:30:51 +0330

    session9-7

--- session9.py
+++ session9.py
@@ -14,28 +14,28 @@ except FileNotFoundError:
          "engine_temp": 150
         } 
 passenger_name = input("what is your name?:")
-print("hi, wellcome passenger_name")
+print(f"hi, wellcome {passenger_name}")
 time.sleep(1)
-print("continue after 2 sec")
+print("continue after 1 sec")
 battery = data["battery"]["current_level"]
-min_capacity = data["battery"]
+min_capacity = data["battery"]["min_capacity"]
 engine_temp = data["engine_temp"]
 while battery  >= 10:
     time.sleep(2)
-    print("continue after 3 sec") 
+    print("continue after 2 sec") 
     print("battery current_level")
     print("every 100 km, low battery 5%")
     if engine_temp > 150:
         print("DANGER! STOP!")
         break
-    passenger_name = input("do you want to stopevery 100 km? (y) or (n)")
+    passenger_name = input("do you want to stope very 100 km? (y) or (n)")
     if passenger_name == "y":
         print("stop!")
         break
     if battery >= 15:
         print("EMERGENCY! please find a charging station.")
-data = battery["current_level"]
-data = ["engine_temp"]
+data["battery"]["current_level"] = battery
+data["engine_temp"] = engine_temp
 with open("car_report2_json" "w") as f:
-    json.dump = (data, f, indent=4)
+    json.dump(data, f, indent=4)
 print("status: data save to file successfully!")
