a9f7975dfe975fec0c1f6c4c350f438b52ce602f
Aima <shabrangfatemeh83@gmail.com>
2026-01-07 12:52:52 +0330

    changes12-2

--- session12.py
+++ session12.py
@@ -9,7 +9,12 @@ try:
         return current_battery - BATTERY_DROP
 except PermissionError:
     print("limited access: initializing default data..")
-    data = {"BATTERY_DROP": 8}
+    data = {
+         "battery": {
+         "min_capacity": 13,     
+         "BATTERY_DROP": 8
+         }
+    } 
        # def battery_distance
 def battery_distance(sensor_battery, distance_traveled):
     return sensor_battery - distance_traveled
@@ -21,7 +26,7 @@ def warning_temp_high(red):
      print("red")
    # name passenger
 def passenger(name):
-    print(f"name: {passenger}")
+    print(f"hi, welcom: {passenger}:")
     #fixed
 CRITICAL_TEMP = data["engine_temp"]["CRITICAL_TEMP"]
 TEMP_RISE = data["engine_temp"]["TEMP_RISE"]
@@ -29,16 +34,44 @@ BATTERY_DROP = data["battery"]["BATTERY_DROP"]
     # variable
 current_temp = 50
 current_battery = data["battery"]["max_capacity"]
-     #
-     
+min_capacity = data["battery"]["min_capacity"]
+     #traveled
+address = input("where is your distination?:")
+print(f"moving: {address}: ")
+car_stop = input("do you whant stop along the way?: (y/n): ").strip().lower()
+      # no address and distance
+stop_address = None
+stop_distance = None
+      # if
+if car_stop in ("y", "yes"):
+        stop_address = input("where do you want to stop?: ")
+        stop_distance = int(input("distance you stop(km): "))
+distance_traveled = 0
+        #loop
+while True:
+    current_temp = sensor_temp(current_temp, TEMP_RISE)
+    current_battery = sensor_battery(current_battery, BATTERY_DROP)
+    sensor_battery = battery_distance(sensor_battery, distance_traveled)
 
+    distance_traveled += 1
 
+    print(f"battery: {current_temp}")
+    print(f"temp: {current_temp}")
+    print(f"sensor_battery: {battery}")
+    print(f"distance_traveled : {distance_traveled} (km)")
 
+    if sensor_temp >= CRITICAL_TEMP:
+    print("warning! stop!")
+    break
+    if sensor_battery <= min_capacity:
+        print("empty battery! stop!")
+        break
+    if stop_distance is not None and distance_traveled >= stop_distance:
+        print(f"stop car at : {stop_address}")
+        break
+time.sleep(1)
 
-while sensor_temp >= CRITICAL_TEMP:
-     
-
-
+data       
 
 
 
