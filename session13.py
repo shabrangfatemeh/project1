c61fd8d439d7777c709c085a184a7149c1d5074e
Aima <shabrangfatemeh83@gmail.com>
2026-01-09 15:20:00 +0330

    changes13-5

--- session13.py
+++ session13.py
@@ -14,17 +14,36 @@ except PermissionError:
         "MAX_TEMP": 150,
         "TEMP_RISE":5,
         "current_temp_air": 25   
-        }
+        },
+        "system_health": 100
     } 
            #  def    smart   sensor    battery   ,   temp
 
-def smart_sensor(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled, current_temp_air):
+def smart_sensor(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled, current_temp_air, REV):
         try:
             distance_factor = 1 + distance_traveled * 0.005
-            new_temp = 1 + (current_temp + TEMP_INCREASE_RATE + current_temp_air) * 0.01
+            REV_factor = 1 + REV * 0.003
+            new_temp = 1 + (current_temp + TEMP_INCREASE_RATE + current_temp_air + REV) * 0.01
             new_battery = current_battery - BATTERY_DROP_RATE * new_temp * distance_factor
             return int(max(new_battery, new_temp, 0))
         except TypeError:
             return current_battery, current_temp
-                                   #    def    name   passenger
-def             
+                                   #    def    WARNING!
+def warning_temp_high():
+    print("RED!!!")
+                                      #           DEf      name   passenger
+def passenger(name):
+    print(f"passenger: {name}:")
+                                # config
+                                            #FIXED
+                                 
+REV = data["engine_temp"]["rev"]
+TEMP_INCREASE_RATE = data["engine_temp"]["TEMP_INCREASE_RATE"]
+BATTERY_DROP_RATE = data["battery"]["BATTERY_DROP_RATE"]
+                                                 # variable                                                                           
+rev = 100
+current_temp = 40
+current_battery = data["battery"]["max_capacity"]
+min_battery = data["battery"]["min_capacity"]
+                           # traveled
+                           
