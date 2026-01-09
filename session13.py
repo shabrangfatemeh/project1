7c834b7f4149f17152a00b4866b144231b22ec87
Aima <shabrangfatemeh83@gmail.com>
2026-01-09 16:54:14 +0330

    changes13-6

--- session13.py
+++ session13.py
@@ -19,7 +19,7 @@ except PermissionError:
     } 
            #  def    smart   sensor    battery   ,   temp
 
-def smart_sensor(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled, current_temp_air, REV):
+def smart_sensor_battery_temp(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled, current_temp_air, REV):
         try:
             distance_factor = 1 + distance_traveled * 0.005
             REV_factor = 1 + REV * 0.003
@@ -46,4 +46,25 @@ current_temp = 40
 current_battery = data["battery"]["max_capacity"]
 min_battery = data["battery"]["min_capacity"]
                            # traveled
-                           
+address = input("where you want the way? : ")
+print(f"moving: {address}:")
+stop_way = input("do you want stop alog the way?: (y/n): ").strip().lower()
+                #       no addresss no distance
+stop_address = None
+stop_distance = None
+                # BET
+if stop_way in ("yes", "y"):
+        stop_address = input("where do you want to stop?: ")
+        stop_distance = int(input("distance you stop?: (km): "))
+distance_traveled = 0
+           #LOOP
+while True:
+    current_battery = smart_sensor_battery_temp(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled, current_temp_air, REV)          
+    current_temp = smart_sensor_battery_temp(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled, current_temp_air, REV)
+    if smart_sensor_battery_temp < 20:
+          
+
+
+
+
+
