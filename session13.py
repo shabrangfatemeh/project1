d3fe15f9f7556b25f90bded1d295bfd458164cd0
Aima <shabrangfatemeh83@gmail.com>
2026-01-09 11:17:07 +0330

    changes13-2

--- session13.py
+++ session13.py
@@ -14,9 +14,16 @@ except PermissionError:
         "MAX_TEMP": 150,
         "TEMP_RISE":5   
         }
-    }        
-    def smart_sensor(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled):
+    } 
+    def smart_sensor_temp(current_temp, TEMP_INCREASE_RATE, distance_traveled, temp_محیط)
         try:
-            factor_distance = 1 + distance_traveled * 0.005
-            factor_temp = 1 + (current_temp + TEMP_INCREASE_RATE + 25) * 0.01
-            factor_battery = current_battery - BATTERY_DROP_RATE * factor_temp *factor_distance
+
+    def smart_sensor_battery(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled):
+        try:
+            distance_factor = 1 + distance_traveled * 0.005
+            new_temp = 1 + (current_temp + TEMP_INCREASE_RATE + 25) * 0.01
+            new_battery = current_battery - BATTERY_DROP_RATE * new_temp * distance_factor
+            return int(max(new_battery, 0))
+            return int(max(new_temp, 0))
+        except TypeError:
+            return current_battery
