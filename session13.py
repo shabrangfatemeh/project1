8f037999e68eaf6c368dccf36b90e85ddd7bd7ac
Aima <shabrangfatemeh83@gmail.com>
2026-02-14 17:31:23 +0330

    changes13-21

--- session13.py
+++ session13.py
@@ -49,59 +49,150 @@ def load_car_data():
         faults = data.get("faults", {})
 
         return{
-             
+            "car_id": car_id,
+            "current_mode": current_mode,
+            "is_active": is_active,
+            "charging": charging,
+            "current_battery": current_battery,
+            "min_battery": min_battery,
+            "max_battery": max_battery,
+            "battery_drop": battery_drop,
+            "critical_battery": critical_battery,
+            "current_enginy_temp": current_engine_temp,
+            "current_air_temp": current_air_temp,
+            "temp_rise": temp_rise,
+            "critical_temp": critical_temp,
+            "oil_level": oil_level,
+            "water_level": water_level,
+            "latitude": latitude,
+            "longitude": longitude,
+            "system_health": system_health,
+            "errors": errors,
+            "faults": faults,
+            "data": data
         }
 
+    except FileNotFoundError:
+        print("Not file! initializing default data...")
+        return None
+    except json.JSONDecodeError:
+        print(f"not file json: {e}")
+        return None
+    except Exception as e:
+        print(f"Unknown error": {e})
+        return None
+#function
+def smart_sensor_battery_temp(current_battery, current_engine_temp, distance_traveled, current_air_temp, battery_drop, temp_rise):
 
-
-
-    current_temp = data["engine_temp"]["current_temp_air"]
-    current_battery = data["battery"]["current_capacity"]
-    oil_level = data["fluids"]["oil_level"]
-    water_level = data["fluids"]["water_level"] 
-
-except PermissionError:
-    print("file promission! initializing default data...")
-    
-    data = {
-        "battery": {
-        "min_capacity": 10,
-        "max_capacity": 200,
-        "CRITICAL_BATTERY": 20
-        },
-        "engine_temp": {
-        "MAX_TEMP": 150,
-        "TEMP_RISE":5,
-        "current_temp_air": 25
-        },
-        "fluids": {
-        "oil_level": 100,
-        "water_level": 100     
-        },
-        "system_health": 100
-       } 
-           #  def    smart   sensor    battery   ,   temp
-
-def smart_sensor_battery_temp(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE, distance_traveled, current_temp_air, REV):
+    """ calculate status battery and temp"""
     try:
-            distance_factor = 1 + distance_traveled * 0.005
-            REV_factor = 1 + REV * 0.003
-            new_temp = current_temp + TEMP_INCREASE_RATE + current_temp_air * 0.01 + REV * 0.01
-            new_battery = current_battery - BATTERY_DROP_RATE * distance_factor  * REV_factor
+#calculate factors influential
+         
+        distance_factor = 1 + distance_traveled * 0.005
+        new_engine_temp = (current_engine_temp + temp_rise + current_air_temp * 0.01)
+        new_battery = current_battery - (battery_drop * distance_factor)
+        return max(new_battery, 0), new_temp
+    except Exception as e:
+        print(f"sensor error: {e}")
+        return current_battery, current_engine_temp
+
+def calculate_system_health(current_battery, current_engine_temp, oil_level, water_level, max_battery, critical_temp):    
+    """calculate all system health"""
+    temp_factor = max(0, 1 - current_engine_temp / critical_temp) 
+    battery_factor = current_battery / max_battery 
+    fluids_factor =  (oil_level + water_level) / 200      #200 = max_oil + max_water
+    system_health = (battery_factor + temp_factor + fluids_factor) / 3 * 100
+    return min(max(int(system_health),0), 100)
+  #   MAIN PROGRAM
+def main():
+    print("\n" + "="*50)
+    print("simulator traveled to car")
+    print("="*50)
+                   
+#load data json
+    car_info = load_car_data()
+    if not car_info:
+        print("program json stop! file chek")
+        return
+    
+#information car show
+        print(f"id: {car_info['car_id']}")
+        print(f"status:{car_info['current_mode']}")
+        print(f"charge:{'yes' if car_info['charging'] else'no'}")
+        print(f"status:{car_info['latitude']},{car_info['longitude']}")
+        print("-"*50)
+
+#information from the user get
+        address = input("where is your destination?")            
+        print(f"moving: {address}")
+
+        stop_way = input("do you have any stope during your trip? : (y/n)").strip().lower()
+
+        stop_distance = None
+        stop_address = None
+
+        if stop_way in ("yes","y"):
+            stop_address = input("where do you want to stop?:")
+            stop_distance = int(input("how many (km) do you have to stop?: "))
+
+      #start trip
+        print("\n"+"="*50)
+        print("start trip")
+        print("="*50)
+        distance_traveled = 0
+        current_battery = car_info['current_battery']
+        current_engine_temp = car_info['current_engine-temp']
+        current_air_temp = car_info['current_air_temp']
+        oil_level = car_info['oil_level']
+        water_level = car_info['water_level']
+        system_health = car_info['system_health']
+
+        #LOOP
+        while distance_traveled < CarConstants.TOTAL_DISTANCE:
+        # UPDATE BATTERY AND TEMP
+
+            current_battery, current_engine_temp = smart_sensor_battery_temp(current_battery, current_engine_temp,
+                distance_traveled, current_air_temp, car_info['battery_drop'], car_info['temp_rise'])        
+
+            distance_traveled = +1
+        #calculate SYSTEM_HEALTH
+            system_health = calaulate_system_health(current_battery, current_engine_temp, oil_level, water_level,
+                                                    car_info['max_battery'], car_info['critical_temp']
+                                                    )    
+        #show status            
+            print(f"\n km: {distance_traveled}")
+            print(f"battery: {current_battery:.1f}%")
+            print(f" engine_temp: {current_engine_temp:.1f}°c")
+            print(f"air_temp: {current_air_temp:.1f}°c")
+            print(f"system_health: {system_health}%")
+
+        #warning temp high
+        if current_engine_temp >= car_info['critical_temp']:
+            print("WARNING! engine overheated")
+            print("STOP EMERGENCY!")
+            break
+        # REVIEW stop distance
+        if stop_distance and distance_traveled == stop_distance:
+            print(f"\n stop: {stop_address}")
+            print(" A RELAX SHORT")
+            time.sleep(3)
+            print("continue travel")
+        #warning battery weak
+            
+
+
+
+
+
               #BET  BATTERY
             if new_battery < data["battery"]["CRITICAL_BATTERY"]:
                 print("CAUTION! battery low (<20%)")
-            return max(new_battery, 0), new_temp
-    except TypeError:
-            return current_battery, current_temp
+           
+   
                                #def health
-def smart_sensor_system_health(current_battery, current_temp, oil_level, water_level):
+def smart_sensor_system_health():
     
-    temp_factor = max(0, 1 - current_temp / data["engine_temp"]["max_temp"]) 
-    battery_factor = current_battery / data["battery"]["max_capacity"] 
-    fluids_factor =  oil_level + water_level / 200 # فرض حداکثر مجموع
-    system_health = (battery_factor + temp_factor + fluids_factor) / 3 * 100
-    return min(max(int(system_health),0), 100)
+    
                                 # config
                                             #FIXd or  # variable
                                                                         
@@ -129,16 +220,15 @@ print("\n===starting trip===")
 total_distance = 10           
 while distance_traveled < total_distance:
 
-    current_battery, current_temp = smart_sensor_battery_temp(current_battery, BATTERY_DROP_RATE, current_temp, TEMP_INCREASE_RATE,
-          distance_traveled, data["engine_temp"]["current_temp_air"], REV)
-    distance_traveled = +1
+    
+    
         # calculathon system_health  محاسبه سلامت سیستم
-    system_health = smart_sensor_system_health(current_battery, current_temp, oil_level, water_level)
+    system_health = smart_sensor_system_health()
     data["system_health"] = system_health
          #نمایش وضعیت
     print(f"\nstep {distance_traveled}:")
-    print(f"battery:{current_battery:.1f}%")       
-    print(f"temp:{current_temp:.1f}°c")
+    print(f"battery:{)       
+    print(f"temp:{")
     print(f"system_health:{system_health}%")
               #bet stop_way 
     if distance_traveled >= total_distance:
@@ -166,3 +256,10 @@ print("status: save file successfully")
 
 
 
+
+
+
+    current_temp = data["engine_temp"]["current_temp_air"]
+    current_battery = data["battery"]["current_capacity"]
+    oil_level = data["fluids"]["oil_level"]
+    water_level = data["fluids"]["water_level"] 
